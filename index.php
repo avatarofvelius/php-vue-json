a483530f0c6902e1db89f3d1b9fd4ceed90d358e
Matt T Myers <avatarofvelius@yahoo.com>
2017-04-23 18:16:52 -0400

    Adding Initial Local Files

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+/*
+$json = '[
+	{
+		"name": "Albert Palenor",
+		"gender": "Male",
+		"birth": "November 12th, 968"
+	},
+	{
+		"name": "Emilia Lancastle",
+		"gender": "Female",
+		"birth": "May 4th, 1286"
+	}
+	]';
+
+// Json Array Object
+$objects = json_decode($json);
+echo $objects[0]->name  . "<br/>";
+echo $objects[0]->birth . "<br/>";
+
+
+echo "<br/>";
+
+// Associative Multi Dimensional Arrays
+$objects = json_decode($json, true);
+echo $objects[1]['name']  . "<br/>";
+echo $objects[1]['birth'] . "<br/>";
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!-- Web Page Layout -->
+	<link rel="stylesheet" media="screen" href="css/index.css" />
+	<link rel="stylesheet" media="screen" href="css/s_.css" />
+	<!-- Script Libraries -->
+	<script type="text/javascript" src="lib/jquery.js"></script>
+
+	<!-- Meta Information -->
+	<title>PHP Template 4192017</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=3.0;">
+	<meta charset="utf-8">
+	<meta name="description" content="PHP Template">
+	<meta name="author" content="Matt T. Myers">
+
+</head>
+
+<body>
+
+	<div id="app">
+		{{ message }}
+	</div>
+
+</body>
+
+	<!-- Script Libraries -->
+	<script src="_library/vue.js"></script>
+	<!-- Scripts -->
+	<script type="text/javascript">
+		var app = new Vue({
+			el: '#app',
+			data: {
+				message: "Hello PHP"
+			}
+		})
+	</script>
+
+</html>
